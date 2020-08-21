5ecd43a6f917f9dc4021333fc8f8897294f2c3ae
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-08-21 13:44:21 -0500

    Updated database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,53 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial NOT NULL,
+	"username" varchar(80) NOT NULL UNIQUE,
+	"password" varchar(1000) NOT NULL,
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "oyster_types" (
+	"id" serial NOT NULL,
+	"global_location" BOOLEAN NOT NULL,
+	"name" varchar(1000) NOT NULL UNIQUE,
+	"description" varchar(1000) NOT NULL,
+	CONSTRAINT "oyster_types_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "inventory" (
+	"user_id" integer NOT NULL,
+	"oyster_types_id" integer NOT NULL,
+	"id" serial NOT NULL,
+	"ship_date" DATE,
+	"last_date_used" DATE,
+	"previous_count" integer NOT NULL,
+	"current_count" integer NOT NULL,
+	"oyster_name" varchar(255) NOT NULL,
+	CONSTRAINT "inventory_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Untitled" (
+
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "inventory" ADD CONSTRAINT "inventory_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "inventory" ADD CONSTRAINT "inventory_fk1" FOREIGN KEY ("oyster_types_id") REFERENCES "oyster_types"("id");
+
+
