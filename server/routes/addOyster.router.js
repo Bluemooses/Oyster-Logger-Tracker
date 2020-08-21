2ac98ea0d69d74c9ee874bd8ba36f75591816a69
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-08-21 16:26:17 -0500

    Basic form using hooks and semantic ui

--- server/routes/addOyster.router.js
+++ server/routes/addOyster.router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
-
 /**
  * GET route template
  */
@@ -10,20 +9,6 @@ router.get("/", (req, res) => {});
 /**
  * POST route template
  */
-router.post("/", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
-  }
-
-  const file = req.files.file;
-  file.mv(`${__dirname}/uploads/images/${file.name}`, (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send(err);
-    }
-
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-  });
-});
+router.post("/", (req, res) => {});
 
 module.exports = router;
--- src/components/InputOyster/InputOyster.js
+++ src/components/InputOyster/InputOyster.js
@@ -1,10 +1,78 @@
-import React from "react";
-import FileUpload from "../FileUpload/FileUpload";
+import React, { useState } from "react";
+import { Button, Checkbox, Form, Radio } from "semantic-ui-react";
+import { func } from "prop-types";
+
 function InputOyster(props) {
+  const [oysterName, setOysterName] = useState("");
+  const [oysterLocation, setOysterLocation] = useState("");
+  const [oysterDescription, setOysterDescription] = useState("");
+  const [coast, setCoast] = useState("");
+
+  function handleRadio(event) {
+    setCoast(event.target.value);
+    console.log(coast);
+  }
+
+  function addOyster() {
+    let oysterObject = {
+      global_location: coast,
+      name: oysterName,
+      description: oysterDescription,
+      geo_location: oysterLocation,
+    };
+    console.log(oysterObject);
+  }
+
   return (
-    <div>
-      <FileUpload />
-    </div>
+    <Form onSubmit={addOyster}>
+      <Form.Field>
+        <label>Oyster Name</label>
+        <input
+          onChange={(e) => {
+            setOysterName(e.target.value);
+          }}
+          placeholder="Oyster Name"
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Location</label>
+        <input
+          onChange={(e) => {
+            setOysterLocation(e.target.value);
+          }}
+          placeholder="Location"
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Description</label>
+        <input
+          onChange={(e) => {
+            setOysterDescription(e.target.value);
+          }}
+          placeholder="Details about taste, wine pairings, etc.."
+        ></input>
+      </Form.Field>
+      <div onChange={handleRadio}>
+        <Form.Group inline>
+          <Form.Field
+            label="East Coast"
+            control="input"
+            type="radio"
+            name="htmlRadios"
+            value={true}
+          />
+          <Form.Field
+            label="West Coast"
+            control="input"
+            type="radio"
+            name="htmlRadios"
+            value={false}
+          />
+        </Form.Group>
+      </div>
+
+      <Button type="submit">Add Oyster</Button>
+    </Form>
   );
 }
 
